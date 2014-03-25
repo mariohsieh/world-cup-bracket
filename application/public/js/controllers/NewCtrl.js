3276d9cd29c293696b14f11c145bf3b00abb1b86
Mario Hsieh <mariohsieh@gmail.com>
2014-03-25 02:46:16 -0700

    group selection functioning

--- application/public/js/controllers/NewCtrl.js
+++ application/public/js/controllers/NewCtrl.js
@@ -1,7 +1,18 @@
 angular.module("NewCtrl", [])
 
 	.controller("NewController", function($scope, Nations) {
+		// page title and description
+		$scope.heading = "Group Stage";
+		$scope.description = "Please choose two nations from each group.";
+		
+		// load nations model into $scope
 		$scope.nations = Nations;
-		$scope.output = "This page allows user to submit a new bracket entry";
-		console.log($scope.nations);
+		$scope.groups = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+		$scope.selectgroup = function(data) {
+			$scope.teams = $scope.nations.getgroup(data);
+			//console.log($scope.teams);
+		}
+		
+		//$scope.teams = $scope.nations.getgroup('A');
 	});
