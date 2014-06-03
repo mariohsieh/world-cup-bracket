8020b40d34fd6f9cca01feea3991e94da1dc604a
Mario Hsieh <mariohsieh@gmail.com>
2014-06-02 17:09:41 -0700

    more styling changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//url: 'mongodb://localhost/worldcup'
+	//url: 'mongodb://localhost/worldcup'		// localhost
 	url: 'mongodb://root:root@ds037778.mongolab.com:37778/worldcup'		// for mongolab
 }
